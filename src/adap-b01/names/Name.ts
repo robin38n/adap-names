912f6da4ca7729ef5516d41db90b4d024e472935
Robin Neubauer <robinneubauer@outlook.de>
2024-10-27 22:10:16 +0100

    Fixed asNameString escape char

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -6,7 +6,7 @@ export class Name {
     private components: string[] = [];
     private delimiter: string = this.DEFAULT_DELIMITER;
 
-    // @method-type Initialization Method
+    /**  @methodtype initialization-method */
     constructor(other: string[], delimiter?: string) {
         this.components = other;
 
@@ -17,7 +17,7 @@ export class Name {
         }
     }
 
-    // @method-type Conversion Method
+    /** @methodtype conversion-method */
     /** Returns human-readable representation of Name instance */
     public asNameString(delimiter: string = this.delimiter): string {
         
@@ -25,14 +25,27 @@ export class Name {
             return "";
         } 
 
-        let str: string = this.components[0] || '';
-        for (let i = 1; i < this.components.length; i++) {
-            str = str.concat(delimiter, this.components[i]);
+        let str: string = "";
+        for (let i = 0; i < this.components.length; i++) {
+            let component = this.components[i];
+            let escapedComponent = "";
+            for (let j = 0; j < component.length; j++) {
+                if (component[j] === this.ESCAPE_CHARACTER) {
+                    // Double the escape character
+                    escapedComponent += this.ESCAPE_CHARACTER + this.ESCAPE_CHARACTER;
+                } else if (component[j] === delimiter) {
+                    // Escape the delimiter
+                    escapedComponent += this.ESCAPE_CHARACTER + delimiter;
+                } else {
+                    escapedComponent += component[j];
+                }
+            }
+            str += (i === 0 ? "" : delimiter) + escapedComponent;
         }
         return str;
     }
 
-    // @method-type Get Method
+    /** @methodtype get-method */
     public getComponent(i: number): string {
 
         if (i < 0 || i >= this.components.length) {
@@ -42,7 +55,7 @@ export class Name {
         return this.components[i];
     }
 
-    // @method-type Set Method
+    /** @methodtype set-method */
     public setComponent(i: number, c: string): void {
 
         if (i < 0 || i >= this.components.length) {
@@ -52,14 +65,13 @@ export class Name {
         this.components[i] = c;
     }
 
-    // @method-type Get Method
+    /** @methodtype get-method */
     /** Returns number of components in Name instance */
     public getNoComponents(): number {
         return this.components.length;
     }
      
-
-    // @method-type Command Method
+    /** @methodtype command-method */
     public insert(i: number, c: string): void {
 
         if (i < 0 || i > this.components.length) {
@@ -69,12 +81,12 @@ export class Name {
         this.components.splice(i, 0, c);
     }
 
-    // @method-type Command Method
+    /** @methodtype command-method */
     public append(c: string): void {
         this.components.push(c);
     }
 
-    // @method-type Command Method
+    /** @methodtype command-method */
     public remove(i: number): void {
 
         if (i < 0 || i >= this.components.length) {
